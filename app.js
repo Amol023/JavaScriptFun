11bc0abf2844b84bad80554b6bbb9d68227ab3db
Amol023 <amol5jsh@gmail.com>
2016-09-06 22:31:36 -0700

    [feat] bubble sort-2

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-console.log('Hello, world!');
+/*************************************************************
+  Bubble sort
+*************************************************************/
+//  Method 1:
+function bubbleSort(array) {
+  const sort = array.slice();
+
+  for (let i = 0; i < sort.length; i++) {
+    for (let j = 0; j < sort.length; j++) {
+      if (sort[j+1] && sort[j] > sort[j + 1]) {
+        let temp = sort[j];
+        sort[j] = sort[j + 1];
+        sort[j + 1] = temp;
+      }
+    }
+  }
+  return sort;
+}
+
+//  Method 2:
+function bubblesort(array) {
+  let sorted = true;
+  for (let i = 0; i < array.length - 1; i++) {
+    if (array[i] > array[i + 1]) {
+      let temp = array[i];
+      array[i] = array[i + 1];
+      array[i + 1] = temp;
+      sorted = false;
+    }
+  }
+  return !sorted ? bubblesort(array) : array;
+}
