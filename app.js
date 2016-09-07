1166541b246f11d9c21e8d51182d6b25e5e3bf13
Amol023 <amol5jsh@gmail.com>
2016-09-06 23:20:10 -0700

    [feat] Replace char with alphabet num

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+'use strict'
 /*************************************************************
   Bubble sort
 *************************************************************/
@@ -86,3 +87,47 @@ console.log(amolID.getID());
     array1.sort((a, b) => a - b); array2.sort((a, b) => a - b);
     return array1.map(v => v * v).every((v, i) => v == array2[i]);
   }
+
+  /*************************************************************
+    Alphabet Position
+  *************************************************************/
+  const alphabetObject = {
+    'a': 1,
+    'b': 2,
+    'c': 3,
+    'd': 4,
+    'e': 5,
+    'f': 6,
+    'g': 7,
+    'h': 8,
+    'i': 9,
+    'j': 10,
+    'k': 11,
+    'l': 12,
+    'm': 13,
+    'n': 14,
+    'o': 15,
+    'p': 16,
+    'q': 17,
+    'r': 18,
+    's': 19,
+    't': 20,
+    'u': 21,
+    'v': 22,
+    'w': 23,
+    'x': 24,
+    'y': 25,
+    'z': 26
+  }
+  function alphabetPosition(text) {
+    const strArr = text.toLowerCase().replace(/\s/g, '').split('');
+    const alphabetNum = [];
+
+    for (let i = 0; i < strArr.length; i++) {
+      if (alphabetObject[strArr[i]]) {
+        alphabetNum.push(alphabetObject[strArr[i]])
+      }
+    }
+    return alphabetNum.join(' ');
+  }
+  console.log(alphabetPosition("The sunset sets at twelve o' clock."))
